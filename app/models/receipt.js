d3c97c6d90bd4055bec4088ec11ab512b4b7beac
Brian <brianallen5193@gmail.com>
2017-02-18 12:09:57 -0500

    Added in date to the receipts

--- app/models/receipt.js
+++ app/models/receipt.js
@@ -4,7 +4,7 @@ export default DS.Model.extend({
   receiptid: DS.attr('string'),
   storeid: DS.attr('string'),
   storename: DS.attr('string'),
-  timestampcreated: DS.attr('number'),
+  timestampcreated: DS.attr(),
   total: DS.attr('number'),
   userid: DS.attr('string')
 });
---
+++ tests/unit/helpers/format-epoch-test.js
@@ -0,0 +1,12 @@
+
+import { formatEpoch } from 'scan-happy-website/helpers/format-epoch';
+import { module, test } from 'qunit';
+
+module('Unit | Helper | format epoch');
+
+// Replace this with your real tests.
+test('it works', function(assert) {
+  let result = formatEpoch([42]);
+  assert.ok(result);
+});
+
