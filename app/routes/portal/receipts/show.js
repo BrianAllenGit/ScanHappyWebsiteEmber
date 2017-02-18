b05e10fc0a0f0c4c042be56ef9296c0bb4849e34
Brian <brianallen5193@gmail.com>
2017-02-17 19:40:11 -0500

    Made the receipts work fully and look much  better

--- app/routes/portal/receipts/show.js
+++ app/routes/portal/receipts/show.js
@@ -2,6 +2,16 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
 	model(params){
-		return this.store.findRecord('receipt',params.receipt_id );
-	}
+		return Ember.RSVP.hash({
+ 	      receipt: this.store.query('receipt', {orderBy: 'receiptid', equalTo: params.receipt_id} ).then(function(items) {
+	      	return items.get('firstObject');
+	      }),
+  	      receiptdetails: this.store.query('receiptdetails', {orderBy: 'receiptid', equalTo: params.receipt_id})
+  	    });
+	},
+	 setupController(controller, model) {
+	    this._super(...arguments);
+	    Ember.set(controller, 'receipt', model.receipt);
+	    Ember.set(controller, 'receiptdetails', model.receiptdetails);
+	  }
 });
