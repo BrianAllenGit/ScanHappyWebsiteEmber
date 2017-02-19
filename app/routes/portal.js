b1791e661d676a7acd9f703cf2457476775b2c75
Brian <brianallen5193@gmail.com>
2017-02-19 11:43:02 -0500

    Cleaning up after the last commit

--- app/routes/portal.js
+++ app/routes/portal.js
@@ -1,16 +1,4 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
-	routeName: true,
-
-
-
-
-
-	 //  function() {
-  //   return this.store.query('user', {equalTo: this.get('session.currentUser.uid') });
-  // },
-  // function() {
-  //   return this.store.query('receipt', {orderBy: 'userid', equalTo: this.get('session.currentUser.uid') });
-  // }
 });
